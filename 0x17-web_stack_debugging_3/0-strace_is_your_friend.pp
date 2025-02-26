e021e53af0e1384c21b705b02f2da199225e69d4
YisehakKEK <yisehakschool@gmail.com>
2025-02-26 17:09:49 +0300

    push

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # A Puppet script to fix a WordPress server error by correcting a typo in the wp-settings.php file
 
 exec { 'fix-wordpress-server-error':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/bin:/bin/',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin:/bin/',
 }
