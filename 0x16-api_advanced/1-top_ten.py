f35b36e5a75a444391b9a6f299a8cef885e13451
YisehakKEK <yisehakschool@gmail.com>
2025-02-26 16:47:08 +0300

    2nd commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module to interact with the Reddit API.
+
+This module provides a function to retrieve and print the titles
+of the first 10 hot posts for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    
+    If the subreddit is invalid, prints None.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Custom-Reddit-API-Request"}
+    params = {"limit": 10}
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 200:
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+    else:
+        print(None)
