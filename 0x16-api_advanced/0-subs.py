1d0a8703a45887038cbbd0f8afacbc6feca45f9b
YisehakKEK <yisehakschool@gmail.com>
2025-02-26 16:24:13 +0300

    Added function to fetch subreddit subscriber count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Function to query the Reddit API and return the number of subscribers
+for a given subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If the subreddit is invalid, returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Custom-Reddit-API-Request"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    return 0
