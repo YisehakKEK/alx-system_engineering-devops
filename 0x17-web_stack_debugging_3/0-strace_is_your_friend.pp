e421ab53458f5e92c1591b8fd2b9482fd2dbb608
YisehakKEK <yisehakschool@gmail.com>
2025-02-26 18:24:31 +0300

    final

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# A Puppet script to fix a WordPress server error by correcting a typo in the wp-settings.php file
+# Create a manifest that fix all termintion of phpp and fixes all bug in wordpress website.
 
-exec { 'fix-wordpress-server-error':
+exec { 'fix_phpp':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin:/bin',
-  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
