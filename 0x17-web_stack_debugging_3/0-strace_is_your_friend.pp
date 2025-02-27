ebc72f646d8b17a6cb77e408dfbf27dcf352320a
YisehakKEK <yisehakschool@gmail.com>
2025-02-27 07:57:06 +0300

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php` I believe that's all the problem that was present.
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+  provider => 'shell',
 }
